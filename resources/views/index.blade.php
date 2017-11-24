331bd9892f9711808136f37aa5c0c18f0a412e1a
kookor1 <kookor1@gmail.com>
2017-11-24 21:46:51 +0900

    Merge branch 'master' of https://github.com/kookor1/laravel

---
+++ resources/views/index.blade.php
@@ -0,0 +1,4 @@
+@extends('master')
+@section('content')
+ your content here!!
+@stop
---
+++ resources/views/master.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang='ko'>
+  <head>
+    <meta charset="utf-8">
+    <title>Laravel 5 </title>
+  </head>
+  <body>
+    @yield('content')
+  </body>
+</html>
